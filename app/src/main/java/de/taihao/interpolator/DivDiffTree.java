070a84d9c7f9eabcf549657053c8ff52dfd28816
zhtaihao <tadamount2@googlemail.com>
2019-12-29 02:30:14 +0100

    implemented getFactorial method, and fixed calculation
    Signed-off-by: zhtaihao <tadamount2@googlemail.com>

--- app/src/main/java/de/taihao/interpolator/DivDiffTree.java
+++ app/src/main/java/de/taihao/interpolator/DivDiffTree.java
@@ -19,6 +19,7 @@ import androidx.annotation.NonNull;
  */
 
 public class DivDiffTree {
+    private static final int MAX_DERIVATIVES = 5;
     private ArrayList<Point> points;
     private int maxM = 0;
 
@@ -42,44 +43,54 @@ public class DivDiffTree {
      * Method to perform the mathematical aspect of the divided difference formula.
      * First checks if entirely identical points were entered;
      * in that case, there is no clearly defined divided difference (division by zero would occur).
-     * Instead it will return the n-th derivative of that single point,
+     * Instead it will return the n-th derivative divided by n! of that single point,
      * where n is the amount of points entered (ref. Wikipedia).
      *
      * @param points an ArrayList of points, may be several identical ones
      * @return returns the calculated divided difference of those points,
-     * outputs the n-th derivative instead if "one" point was entered n times
+     * outputs the n-th derivative divided by n! instead if "one" point was entered n times
      */
     public double getDivDiff(@NonNull ArrayList<Point> points) throws IllegalArgumentException {
-        int amountOfPoints = points.size();
+        int n = points.size();
         double result = 0;
 
         boolean isAllPointsEqual = true;
-        for (Point p : points) {
+        for (Point p : points)
             if (!p.equals(points.get(0))) {
                 isAllPointsEqual = false;
                 break;
             }
-        }
 
-        if (isAllPointsEqual) {
-            return points.get(0).getF()[amountOfPoints];
-        } else {
-            for (Point p : points) {
-                double denominator = 1;
-                for (int k = 0; k < amountOfPoints; k++) {
-                    if (k != points.indexOf(p)) {
-                        denominator *= (p.getX() - points.get(k).getX());
-                    }
-                }
-                if (denominator == 0.0) {
-                    throw new IllegalArgumentException("The denominator is 0 and division can't be performed.");
-                } else {
-                    result += p.getF()[0] / denominator;
-                }
-            }
+        if (isAllPointsEqual) return points.get(0).getF()[n] / getFactorial(n);
+        else for (Point p : points) {
+            double denominator = 1;
+            for (int k = 0; k < n; k++)
+                if (k != points.indexOf(p)) denominator *= (p.getX() - points.get(k).getX());
+            if (denominator == 0.0)
+                throw new IllegalArgumentException("The denominator is 0 and division can't be performed.");
+            else result += p.getF()[0] / denominator;
         }
         return result;
     }
 
-
+    /**
+     * @param k Integer to calculate the factorial of
+     * @return returns k! = k*(k-1)!; 0!=1; 1!=1
+     * @throws IllegalArgumentException thrown when a negative number
+     *                                  or a number larger than the maximum amount of expected derivatives is entered.
+     *                                  MAX_DERIVATIVES is around 5 which should be plenty tbh
+     */
+    private int getFactorial(int k) throws IllegalArgumentException {
+        int result = 1;
+        if (k < 0)
+            throw new IllegalArgumentException(k + "is a negative number, factorial can't be computed.");
+        else if (k > MAX_DERIVATIVES)
+            throw new IllegalArgumentException(k + "is too large to calculate a factorial.");
+        else if (k == 0) return 1;
+        else if (k == 1) return 1;
+        else {
+            for (int i = 1; i < k + 1; i++) result *= i;
+            return result;
+        }
+    }
 }
